2b8004aa53d484e03205ee9a52225cc185bdf611
wisnizza <126265612+wisnizza@users.noreply.github.com>
2023-03-22 09:47:45 -0700

    Added block07 Workshop: Secure the Vault

---
+++ main.js
@@ -0,0 +1,17 @@
+//Steps on Secure The Vault Workshop
+
+// 1. Create a file index.html
+// 2. Put a Header
+// 3. Open it in Chrome
+// 4. Inspect and open console
+// 5. Create a file main.js
+// 6. Add an external script file to an HTML file
+// 7. Create a string saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 8. Assign 3 variables that corresponds the result to the combination of the lock: num1 = 5 + 5, num2 = 42 - 2, num3 = 13 * 3
+// 9. Create a popup dialog displaying the vault codes and the Text.
+
+const x = ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+let num1 = 5 + 5;
+let num2 = 42 - 2;
+let num3 = 13 * 3;
+alert(x + " " + num1 + "-" + num2 + "-" + num3);
